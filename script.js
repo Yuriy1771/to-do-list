291ec3cd461a1e60d1cb8bdc67567c8e64634d88
Yuriy <dementev.yuriy@list.ru>
2022-04-13 19:47:24 +0300

    добавил возможность удалить задачу даблкликом или удалить все задачи при нажатии на trashBtn, изменил height на auto, добавил валидацию на inpToDo если true то задача добавляется, иначе inpToDo подчеркивается красным

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 let inpToDo = document.querySelector('.inpToDo');
 let btnAdd = document.querySelector('.btnAdd');
-let list = document.querySelector('.list'); 
+let list = document.querySelector('.list');
+
+//кнопка мусор
+let trashBtn = document.querySelector('.trashBtn');
 
 btnAdd.onclick = () => {
     addItem()
@@ -14,13 +17,27 @@ inpToDo.addEventListener('keydown', function(event) {
 
 function addItem() {
     let inpValue = inpToDo.value;
-    let h3 = document.createElement('h3');
-    h3.innerHTML += '- ' + inpValue;
-
-    h3.onclick = () => {
-        h3.style.textDecoration = 'line-through';
-        h3.style.color = 'rgb(33, 167, 0)';
+    if(inpValue != '') {
+        inpToDo.style.borderBottom = '1px solid black'
+        let h3 = document.createElement('h3');
+        h3.innerHTML += '- ' + inpValue;
+    
+        h3.onclick = () => {
+            h3.style.textDecoration = 'line-through';
+            h3.style.color = 'rgb(33, 167, 0)';
+        }
+    
+        h3.ondblclick = () => {
+            h3.style.display = 'none';
+        }
+    
+        list.insertAdjacentElement('beforeend', h3);
+        inpToDo.value = '';
+    }else {
+        inpToDo.style.borderBottom = '1px solid red';
     }
-    list.insertAdjacentElement('beforeend', h3);
-    inpToDo.value = '';
+}
+
+trashBtn.onclick = () => {
+    list.innerHTML = '';
 }
